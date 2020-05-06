4d5d70e0cf0ea75a92c7f83d68ac0b06118d1616
Aaron Schuyler <asky28@gmail.com>
2020-05-06 17:53:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(line)
       "#{count}. #{person}"
     end
     
-    puts "The line is currently #{line.each{|person| puts person}}"
+    puts "The line is currently #{line.each{|person| person}}"
   end
 end
 
