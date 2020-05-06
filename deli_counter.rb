b7194e6cddc621cfa5b894f872d4ed2372a0fda2
Aaron Schuyler <asky28@gmail.com>
2020-05-06 17:52:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require "pry"
+
+def line(line)
+  if line.length() == 0
+    puts "The line is currently empty."
+  else
+    count = 0
+    line.collect do |person|
+      count +=1
+      "#{count}. #{person}"
+    end
+    binding.pry
+    puts "The line is currently #{line.each{|person| person}}"
+  end
+end
+
+def take_a_number(line, name)
+  
+end
+
+def now_serving(line)
+  
+end
